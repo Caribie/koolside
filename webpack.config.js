3c81193aa85f711946328bdca35d4f3942b1ca43
Sangha Lee <totoriato@gmail.com>
2019-12-31 15:29:55 +0900

    캐시 문제로 다운로드 주소 변경

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = (env, argv) => {
   // @namespace     https://github.com/toriato/koolside
   // @supportURL    https://github.com/toriato/koolside/issues
   // @homepageURL   https://github.com/toriato/koolside
-  // @downloadURL   https://derpy.kr/koolside/koolside.user.js
+  // @downloadURL   https://derpy.kr/koolside/download.php
   // @version       ${package.version}
   // @author        ${package.author}
   // @description   ${package.description}
